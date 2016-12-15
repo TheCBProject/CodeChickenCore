c52c17aa4c651f6cd4ececc29f1d001ec3fbdcd9
covers1624 <laughlan.cov@internode.on.net>
2016-12-16 10:00:58 +1030

    Jar signing and fingerprint verification.

--- src/main/java/codechicken/core/launch/CodeChickenCorePlugin.java
+++ src/main/java/codechicken/core/launch/CodeChickenCorePlugin.java
@@ -8,13 +8,14 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.FMLInjectionData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
-@Mod(modid = "CodeChickenCore", name = "CodeChicken Core", dependencies = "required-after:CodeChickenLib@[" + CodeChickenLib.version + ",)", acceptedMinecraftVersions = CodeChickenLib.mcVersion)
+@Mod(modid = "CodeChickenCore", name = "CodeChicken Core", dependencies = "required-after:CodeChickenLib@[" + CodeChickenLib.version + ",)", acceptedMinecraftVersions = CodeChickenLib.mcVersion, certificateFingerprint = "f1850c39b2516232a2108a7bd84d1cb5df93b261")
 public class CodeChickenCorePlugin {
     public static final String version = "${mod_version}";
 
@@ -40,6 +41,11 @@ public class CodeChickenCorePlugin {
 
         DepLoader.load();
         loadConfig();
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        FingerprintChecker.runFingerprintChecks();
     }
 
     @EventHandler
